57965b838ac42fc56379c1036eb2a3c3cbc7f267
deviengeorge <deviengeorge99@gmail.com>
2021-04-19 19:18:14 +0200

    added pdf printing to leaders

--- app/Http/Controllers/LeadersExportController.php
+++ app/Http/Controllers/LeadersExportController.php
@@ -27,6 +27,8 @@ class LeadersExportController extends Controller
 
     public function exportPDF()
     {
+        ini_set('max_execution_time', 300);
+        ini_set("memory_limit", "512M");
         $data = Leader::all();
         // echo $data;
         $pdf = PDF::loadView('leaders', ['data' => $data]);
